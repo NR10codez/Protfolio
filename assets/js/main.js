5671def37239ba557cebf9186185b0c2e92620fc
NR10codez <aparnanr050@gmail.com>
2024-11-22 13:06:31 +0530

    add myproject

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,8 @@
-/**
-* Template Name: iPortfolio
-* Updated: May 30 2023 with Bootstrap v5.3.0
-* Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
-  /**
-   * Easy selector helper function
-   */
+
   const select = (el, all = false) => {
     el = el.trim()
     if (all) {
@@ -345,5 +337,5 @@ function validateForm() {
     return false;
   }
   
-  return true;
-      }
+  return true;
+ }
